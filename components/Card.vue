c610b981ae2a05ced00a61abd9793fd28dc9a74d
takuma <tackman@tackman.info>
2018-12-03 19:58:22 +0900

    basic card layout template

---
+++ components/Card.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="Card">
+    <div class="cost">
+      {{ cost }}
+    </div>
+    <div class="card-name">
+      {{ cardName }}
+    </div>
+    <div class="card-body">
+      {{ cardBody }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cost: {
+      type: String,
+      default: '10'
+    },
+    cardName: {
+      type: String,
+      default: 'card-name'
+    },
+    cardBody: {
+      type: String,
+      default: 'card-body'
+    }
+  }
+}
+</script>
+
+<style>
+.cost {
+  float: left;
+  font-size: 1cm;
+  font-style: bold;
+}
+
+.card-name {
+  float: center;
+  font-size: 0.8cm;
+}
+
+.card-body {
+  float: none;
+  margin-top: 1cm;
+  font-size: 0.5cm;
+}
+
+.Card {
+  margin: 0.5cm;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="container">
+    <div class="card-grid-container">
+      <Card v-bind="{cost:'123', cardName:'First Card', cardBody:'Some explanations here'}" />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card.vue'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.card-grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 33%);
+  grid-template-rows: repeat(3, 33%);
+  width: 21cm;
+  height: 29.7cm;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
